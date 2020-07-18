f43ec3ea9d56f8b825dd35eac0142a09ac0d1a5f
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-07-18 08:10:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
       else
         @status = "rejected"
         return "Transaction rejected. Please check your account balance."
-  
+
       end
     end
     def reverse_transfer
@@ -31,4 +31,4 @@ class Transfer
         @status = "reversed"
       end
     end
-  end	end
+  end	endss
